26cc9975924d91d119c1ae9e969c31c0cd23529f
HARSHIT BANSAL <harshit.bansal1236@gmail.com>
2024-06-18 00:11:24 +0530

    src change

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -8,7 +8,7 @@ export default function Weather({weather}){
             <h2>{weather.name}</h2>
             <hr style={{color:"white"}}/>
             <div className="temp">
-                <img src="../public/temp.svg" alt="Temp" className="tempSVG"/>
+                <img src="/temp.svg" alt="Temp" className="tempSVG"/>
                 <div className="temp2">
                 <h1>{Math.round(weather.temp)}&deg;</h1>
                 <h2>{weather.weather}</h2>
@@ -16,10 +16,10 @@ export default function Weather({weather}){
             </div>
             <br /><hr style={{color:"white"}}/>
             <div className="subWeather">
-                <SubWeather data={weather.wind} unit={"km/h"} src={"../public/wind.svg"} name={"Wind"}/>
-                <SubWeather data={weather.clouds} unit={"%"} src={"../public/cloud.svg"} name={"Clouds"}/>
-                <SubWeather data={weather.pressure} unit={"mbar"} src={"../public/temp.svg"} name={"Pressure"}/>
-                <SubWeather data={weather.humidity} unit={"%"} src={"../public/humidity.svg"} name={"Humidity"}/>
+                <SubWeather data={weather.wind} unit={"km/h"} src={"/wind.svg"} name={"Wind"}/>
+                <SubWeather data={weather.clouds} unit={"%"} src={"/cloud.svg"} name={"Clouds"}/>
+                <SubWeather data={weather.pressure} unit={"mbar"} src={"/temp.svg"} name={"Pressure"}/>
+                <SubWeather data={weather.humidity} unit={"%"} src={"/humidity.svg"} name={"Humidity"}/>
             </div>
         </div>
     )
